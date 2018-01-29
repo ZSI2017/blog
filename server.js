aa39d536d49987badbda369b22c444e1d2f599ac
liqi <liqi@bangbangda.cn>
2018-01-29 17:50:54 +0800

    vue2.x总结 vue-router

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs = require('fs');
+var file = 'hello.html'
+var index = fs.readFileSync(file);
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(index);
+}).listen(8000);
