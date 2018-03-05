f955665d92e98b5af35feded18a63c3dae8239de
liqi <liqi@bangbangda.cn>
2018-03-05 17:44:11 +0800

    add javascript语言精粹 函数（二）

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var file = 'hello.html'
 var index = fs.readFileSync(file);
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(index);
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.end(index)
 }).listen(8000);
